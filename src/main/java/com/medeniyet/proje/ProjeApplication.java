d04287d75b0ba9178f07416cbb69b0c3a3bddc59
Muhammet Emin ZEYREKLİ <20120205050@ismu.edu.tr>
2022-10-16 22:44:48 +0300

    ProjeAplication.java has been updated

--- src/main/java/com/medeniyet/proje/ProjeApplication.java
+++ src/main/java/com/medeniyet/proje/ProjeApplication.java
@@ -2,12 +2,23 @@ package com.medeniyet.proje;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin
 public class ProjeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjeApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		String msg = "hello";
+		return msg;
 	}
 
 }
